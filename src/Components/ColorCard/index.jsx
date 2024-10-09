ecd1021cf2b0c221ae95a17c14fd208f05dc8215
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-10-09 19:53:29 +0200

    finish7

--- src/Components/ColorCard/index.jsx
+++ src/Components/ColorCard/index.jsx
@@ -1,12 +1,32 @@
 import "./Color.css";
 import Button from "../Button/Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ColorForm from "../ColorForm/ColorForm";
 import CopyToClipboard from "../CopyToClipboard";
 export default function ColorCard({ color, onDeleteColor, onEditColor }) {
   const [confirmDelete, setConfirmDelete] = useState(false);
   const [isEditMode, setIsEditMode] = useState(false);
+  const [contrast, setContrast] = useState(null);
 
+  useEffect(() => {
+    async function postFetch() {
+      const response = await fetch(
+        "https://www.aremycolorsaccessible.com/api/are-they",
+        {
+          method: "POST",
+          body: JSON.stringify({ colors: [color.hex, color.contrastText] }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      console.log("data: ", data);
+
+      setContrast(data.overall);
+    }
+    postFetch();
+  }, [color]);
   return (
     <div
       className="color-card"
@@ -19,6 +39,7 @@ export default function ColorCard({ color, onDeleteColor, onEditColor }) {
       <CopyToClipboard hex={color.hex} />
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      <p>Overall contrast score: {contrast}</p>
 
       {isEditMode ? (
         <>
