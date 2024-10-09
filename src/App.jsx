8f5a2f92901a41083075aa68f61093cc142e85f1
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-10-09 17:34:22 +0200

    finish3

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,26 @@ function App() {
   function handleAddColor(color) {
     setColors([color, ...colors]);
   }
+  function handleDeleteColor(id) {
+    setColors(colors.filter((color) => color.id !== id));
+  }
   return (
     <>
       <h1>Theme Creator</h1>
       <ColorForm onAddColor={handleAddColor} />
-      {colors.map((color) => {
-        return <ColorCard key={color.id} color={color} />;
-      })}
+      {colors.length === 0 ? (
+        <p>no colors in pallett. Please create some</p>
+      ) : (
+        colors.map((color) => {
+          return (
+            <ColorCard
+              key={color.id}
+              color={color}
+              onDeleteColor={handleDeleteColor}
+            />
+          );
+        })
+      )}
     </>
   );
 }
--- src/Components/ColorCard/index.jsx
+++ src/Components/ColorCard/index.jsx
@@ -1,6 +1,9 @@
 import "./Color.css";
+import Button from "../Button/Button";
+import { useState } from "react";
+export default function ColorCard({ color, onDeleteColor }) {
+  const [confirmDelete, setConfirmDelete] = useState(false);
 
-export default function ColorCard({ color }) {
   return (
     <div
       className="color-card"
@@ -12,6 +15,27 @@ export default function ColorCard({ color }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      {confirmDelete ? (
+        <>
+          <span className="color-card-headline">really delete?</span>
+          <Button
+            onClick={() => {
+              setConfirmDelete(false);
+            }}
+          >
+            CANCEL
+          </Button>
+          <Button onClick={() => onDeleteColor(color.id)}>DELETE</Button>
+        </>
+      ) : (
+        <Button
+          onClick={() => {
+            setConfirmDelete(true);
+          }}
+        >
+          DELETE
+        </Button>
+      )}
     </div>
   );
 }
